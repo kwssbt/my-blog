2bc2ff71d55309a4bfaf5b48c31e336ba492547c
Zhang <3393019220@qq.com>
2026-03-14 22:49:35 +0800

    最短路

---
+++ posts/2026/03/对拍器/rand.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+template<typename T>T get_rand(T l, T r) {
+    uniform_int_distribution<T> dis(l, r);
+    return dis(rng);
+}
+
+template<typename T>void pri_x(T l, T r) {
+    uniform_int_distribution<long long> dis(l, r);
+    cout<<dis(rng);
+}
+
+template<typename T>void pri_x_op(T l, T r, char op) {
+    uniform_int_distribution<long long> dis(l, r);
+    cout<<dis(rng)<<op;
+}
+
+template<typename T>void pri_arr(int len, T l, T r) {
+    uniform_int_distribution<long long> dis(l, r);
+    for (int i = 1; i < len; i++) {
+    cout<<dis(rng)<<' ';
+    }
+    cout<<dis(rng)<<'\n';
+}
+
+int main() {
+
+}
